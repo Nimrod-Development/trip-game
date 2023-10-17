032633db1c7363e57f3d77a130787f0b7f9f6f32
Huzza27 <samfarnsley@gmail.com>
2023-10-17 11:31:05 -0700

    Fixing spamming of messages when claiming land

---
+++ src/main/java/net/nimrod/tripgame/BossBarTimer.java
@@ -0,0 +1,74 @@
+package net.nimrod.tripgame;
+
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BossBarTimer {
+    private final JavaPlugin plugin;
+    private BossBar timerBar;
+    private int taskID;
+
+    public BossBarTimer(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public boolean startTimer(Player player, int durationInSeconds) {
+        if (timerBar != null) {
+            timerBar.removeAll(); // Remove all players from the existing boss bar (if it exists) before creating a new one
+        }
+
+        // Create a new boss bar
+        timerBar = Bukkit.createBossBar("Timer", BarColor.RED, BarStyle.SOLID);
+        timerBar.addPlayer(player); // Add the player to see this boss bar
+
+        taskID = Bukkit.getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
+            private int secondsLeft = durationInSeconds;
+
+            @Override
+            public void run() {
+                if (secondsLeft <= 0) {
+                    // When timer finishes
+                    Bukkit.getScheduler().cancelTask(taskID); // Cancel this repeating task
+                    timerBar.removeAll(); // Remove all players from this boss bar to hide it
+                    return;
+                }
+
+                // Update the progress on the boss bar
+                double progress = (double) secondsLeft / durationInSeconds;
+                timerBar.setProgress(progress);
+
+                // Decrement the seconds left
+                secondsLeft--;
+            }
+        }, 0L, 20L); // 20L means the run() method will be called every second (20 ticks = 1 second in Minecraft)
+        return true;
+    }
+
+    public boolean stopTimer() {
+        System.out.println("Attempting to stop the timer."); // Debug message
+
+        if (taskID != -1) {
+            System.out.println("Cancelling the task with ID: " + taskID); // Debug message
+            Bukkit.getScheduler().cancelTask(taskID);
+            taskID = -1;
+        } else {
+            System.out.println("No task to cancel."); // Debug message
+        }
+
+        if (timerBar != null) {
+            System.out.println("Removing all players from the timer bar."); // Debug message
+            timerBar.removeAll();
+            timerBar = null;
+        } else {
+            System.out.println("No active timer bar to remove players from."); // Debug message
+        }
+        return false;
+    }
+
+
+}
+
--- src/main/java/net/nimrod/tripgame/Tripgame.java
+++ src/main/java/net/nimrod/tripgame/Tripgame.java
@@ -1,5 +1,8 @@
 package net.nimrod.tripgame;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
@@ -10,26 +13,50 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.units.qual.A;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
 
 public class Tripgame extends JavaPlugin {
 
     private final String TOTEM_TAG = "totem";
     private final double TOTEM_RADIUS_SQUARED = 4 * 4;
+    public ArrayList<Player> players = new ArrayList<>();
+    private BossBarTimer timer;
+    boolean claiming = false;
+    boolean claimTimerRunning = false;
 
     @Override
     public void onEnable() {
+        timer = new BossBarTimer(this);
         this.getCommand("claim").setExecutor(new ClaimCommandExecutor());
         this.getCommand("totem").setExecutor(new TotemCommandExecutor());
+        this.getCommand("player").setExecutor(new MakePlayer());
 
         // Repeating task to check player proximity to the totem
         this.getServer().getScheduler().runTaskTimer(this, this::checkPlayersNearTotem, 0, 20); // Check every second
     }
 
     private void checkPlayersNearTotem() {
-        for (Player player : getServer().getOnlinePlayers()) {
-            for (ArmorStand armorStand : player.getWorld().getEntitiesByClass(ArmorStand.class)) {
-                if (armorStand.hasMetadata(TOTEM_TAG) && player.getLocation().distanceSquared(armorStand.getLocation()) <= TOTEM_RADIUS_SQUARED) {
-                    player.sendMessage("You are trying to claim land");
+        if(players != null) {
+            for (Player player : getServer().getOnlinePlayers()) {
+                for (ArmorStand armorStand : player.getWorld().getEntitiesByClass(ArmorStand.class)) {
+                    if (players.contains(player)) {
+                        if (armorStand.hasMetadata(TOTEM_TAG) && player.getLocation().distanceSquared(armorStand.getLocation()) <= TOTEM_RADIUS_SQUARED && claiming == false) {
+                            claiming = true;
+                            player.sendTitle("Claiming...", null, 10, 20, 10);
+                            if(!claimTimerRunning)
+                                claimTimerRunning = timer.startTimer(player, 10);
+                            break;
+                        } else if (armorStand.hasMetadata(TOTEM_TAG) && player.getLocation().distanceSquared(armorStand.getLocation()) > TOTEM_RADIUS_SQUARED && claiming == true) {
+                            claiming = false;
+                            claimTimerRunning = timer.stopTimer();
+                            break;
+                        }
+                    }
                 }
             }
         }
@@ -52,7 +79,7 @@ public class Tripgame extends JavaPlugin {
             for (int x = centerX - 5; x <= centerX + 5; x++) {
                 for (int z = centerZ - 5; z <= centerZ + 5; z++) {
                     if ((x - centerX) * (x - centerX) + (z - centerZ) * (z - centerZ) <= radiusSquared) {
-                        player.getWorld().getBlockAt(x, centerY, z).setType(Material.LIME_STAINED_GLASS);
+                        player.getWorld().getBlockAt(x, centerY, z).setType(Material.WHITE_STAINED_GLASS);
                     }
                 }
             }
@@ -83,8 +110,20 @@ public class Tripgame extends JavaPlugin {
                     for (ArmorStand armorStand : player.getWorld().getEntitiesByClass(ArmorStand.class)) {
                         if (armorStand.hasMetadata(TOTEM_TAG)) {
                             armorStand.remove();
+
+                            int x = armorStand.getLocation().getBlockX();
+                            int y = armorStand.getLocation().getBlockY();
+                            int z = armorStand.getLocation().getBlockZ();
+
+
+                            for (int dx = -1; dx <= 1; dx++) {
+                                for (int dz = -1; dz <= 1; dz++) {
+                                    armorStand.getWorld().getBlockAt(x + dx, y - 1, z + dz).setType(Material.WHITE_WOOL);
+                                }
+                            }
                             player.sendMessage("Totem removed!");
                             break; // Break after removing one totem. If you have multiple totems and want to remove all at once, you can remove this line.
+
                         }
                     }
                 }
@@ -94,8 +133,55 @@ public class Tripgame extends JavaPlugin {
         }
     }
 
-    public void spawnTotem(Player player, ArmorStand totem)
-    {
+        private class MakePlayer implements CommandExecutor {
+            @Override
+            public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+                if (!(sender instanceof Player)) {
+                    sender.sendMessage("This command can only be used by a player!");
+                    return true;
+                }
+
+                if (args.length == 0) {
+                    sender.sendMessage("Usage: /player <add/remove/list>");
+                    return true;
+                }
+
+                Player player = (Player) sender;
+                if (args[0].equalsIgnoreCase("add")) {
+                    if (!players.contains(player)) {
+                        players.add(player);
+                        sender.sendMessage("You have been added to the list!");
+                    } else {
+                        sender.sendMessage("You are already in the list!");
+                    }
+                } else if (args[0].equalsIgnoreCase("remove")) {
+                    if (players.contains(player)) {
+                        players.remove(player);
+                        sender.sendMessage("You have been removed from the list!");
+                    } else {
+                        sender.sendMessage("You are not in the list!");
+                    }
+                } else if (args[0].equalsIgnoreCase("list")) {
+                    if (players.isEmpty()) {
+                        sender.sendMessage("The list is empty.");
+                    } else {
+                        StringBuilder playerNames = new StringBuilder("Players in the list: ");
+                        for (Player p : players) {
+                            playerNames.append(p.getName()).append(", ");
+                        }
+                        // Remove the last comma and space
+                        sender.sendMessage(playerNames.substring(0, playerNames.length() - 2));
+                    }
+                } else {
+                    sender.sendMessage("Invalid argument. Use add, remove, or list.");
+                }
+
+                return true;
+            }
+        }
+
+
+    public void spawnTotem(Player player, ArmorStand totem) {
         totem.setMetadata(TOTEM_TAG, new FixedMetadataValue(Tripgame.this, true));
         player.sendMessage("Totem set at your location!");
         // Coordinates of the player (or armor stand since they share the same location)
@@ -109,7 +195,5 @@ public class Tripgame extends JavaPlugin {
                 player.getWorld().getBlockAt(x + dx, y - 1, z + dz).setType(Material.LIME_STAINED_GLASS);
             }
         }
-
-
     }
 }
